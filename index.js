075b4c067e521730805301d3ef2853514b02259a
Ali <ali.t.a36360@gmail.com>
2024-09-22 21:30:00 -0400

    got basic http methods to work + added middleware

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('static-content'));
+app.use(express.json()); // Middleware to parse JSON data in request bodies
+app.use(express.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.static('static-content')); // Serve static files from 'static-content' directory
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/static-content/index.html');
   });
+
+
+app.post('/submit', (req, res) => {
+  const name = req.body.name;
+  const age = req.body;
+  console.log(name);
+
+  // Process form data (e.g., log it or send an email)
+  // console.log("here");\
+  // res.send(name);
+  res.send(age);
+});
+
+
+app.get('/testing', (req, res) => {
+  res.send("200!!!!!!!!!!");
+})
 
 
 app.listen(port, () => {
--- static-content/js/app.js
+++ static-content/js/app.js
@@ -1,15 +1,57 @@
 // JavaScript to handle form submission
-document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevents form from submitting the traditional way
+// document.getElementById('contact-form').addEventListener('submit', function (event) {
+//     event.preventDefault(); // Prevents form from submitting the traditional way
   
-    const name = document.getElementById('name').value;
-    const message = document.getElementById('message').value;
+//     const name = document.getElementById('name').value;
+//     testF();
+//     // const message = document.getElementById('message').value;
   
-    // Basic form validation
-    if (name && message) {
-      alert('Message sent! Thank you, ' + name + '.');
-    } else {
-      alert('Please fill in both fields.');
-    }
+//     // Basic form validation
+//     // if (name) {
+//     //   alert('Message sent! Thank you, ' + name + '.');
+//     // } else {
+//     //   alert('Please fill in both fields.');
+//     // }
+//   });
+
+function clicked(){
+  console.log("button clicked!");
+  // testF();
+  posttest();
+}
+
+function testF(){
+  $.ajax({
+    method: "GET",
+    url: '/testing',
+    processData: false,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json"
+  })
+  .done(function(data, textStatus, jqXHR) {
+    console.log(jqXHR.status + " " + textStatus); 
+    console.log("Server Response: " + JSON.stringify(data));
+  })
+  .fail(function(err) {
+    console.log("Request failed. Status: " + err.status + ", Response: " + JSON.stringify(err.responseJSON));
   });
+}
+
+function posttest(){
+  $.ajax({
+    method: "POST",
+    url: '/submit',
+    data: JSON.stringify({name: "Ali", age: "21"}),
+    processData: false,
+    contentType: "application/json; charset=utf-8",
+    dataType: "json"
+  })
+  .done(function(data, textStatus, jqXHR) {
+    console.log(jqXHR.status + " " + textStatus); 
+    console.log("Server Response: " + JSON.stringify(data));
+  })
+  .fail(function(err) {
+    console.log("Request failed. Status: " + err.status + ", Response: " + JSON.stringify(err.responseJSON));
+  });
+}
   
